59f67721ae00cf4146b15e3ac8c4d82505194ca9
Madeline Stark <madeline.stark1@gmail.com>
2018-09-10 16:18:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,4 +2,11 @@ class Application < Sinatra::Base
     get '/' do
       erb :index
     end
+
+    #for now only repsonds to /, write below to respond to /greet
+
+    post '/greet' do
+      erb :greet
+    end
+
 end
